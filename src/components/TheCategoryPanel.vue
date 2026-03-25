ea47a49924ed215c2e48684b2e15c13e5305106a
kookywarrior <kookywarrior@gmail.com>
2026-03-25 22:39:50 +0800

    feat: added padding to panel

--- src/components/TheCategoryPanel.vue
+++ src/components/TheCategoryPanel.vue
@@ -10,7 +10,7 @@ const categoryStore = useSettingsStore()
 <template>
   <Panel>
     <label v-for="options in categoryData.categoryOptions" :key="options.value"
-      class="w-full block text-center text-xl px-3 mb-3 first:mt-3 last:mb-4 hover:text-gray-500 cursor-pointer">
+      class="w-full block text-center text-2xl mb-4 last:mb-1 hover:text-gray-500 cursor-pointer">
       <div :class="{
         'border-l-4 border-gray-800': categoryStore.selectedCategory === options.value
       }">{{ options.label }}</div>
--- src/components/base/Panel.vue
+++ src/components/base/Panel.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-white h-fit shadow-(--shadow-bottom) rounded-lg">
+  <section class="bg-white h-fit shadow-(--shadow-bottom) rounded-lg p-4">
     <slot />
   </section>
 </template>
